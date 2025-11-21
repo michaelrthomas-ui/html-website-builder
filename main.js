a7549b105b4a6a306b23891dba7018730fe392ce
michaelrthomas-ui <michaelrthomas@gmail.com>
2025-11-21 10:26:05 -0500

    Updated main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import JSZip from 'jszip';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const BUCKET_NAME = 'sites';
